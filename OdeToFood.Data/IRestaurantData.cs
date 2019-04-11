c9a39b0c0dd29aee2a8db9a608ed733a6d64984c
Russell.Frame <russell.frame@filmtrack.net>
2019-04-11 14:19:33 -0700

    Drilling into data complete

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+        //private List<string> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant
+                {
+                    Id = 1,
+                    Name = "Scott's Pizza",
+                    Location = "Maryland",
+                    Cuisine = CuisineType.Italian
+                },
+                new Restaurant
+                {
+                    Id = 2,
+                    Name = "Cinnamon CLub",
+                    Location = "London",
+                    Cuisine = CuisineType.None
+                },
+                new Restaurant
+                {
+                    Id = 3,
+                    Name = "La Costa",
+                    Location = "California",
+                    Cuisine = CuisineType.Mexican
+                }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -1,20 +1,28 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class List : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public List(IConfiguration config)
+        public List(IConfiguration config,
+                    IRestaurantData restaurantData)
         {
             _config = config;
+            _restaurantData = restaurantData;
         }
         public void OnGet()
         {
             Message = _config["Message"];
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
